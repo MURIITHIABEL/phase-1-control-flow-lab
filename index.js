981627f4a29fcf18ba4df6a4659cd597e2964200
Abel Muriithi <abelmuriithi04@gmail.com>
2022-11-26 14:44:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(fare){
+  if (fare <= 400){
+    return "This one is on me!";
+ }
+ else if (fare > 2000 && fare < 2500){
+ return "I will gladly take your thirty bucks.";
+ }
+ else if(fare > 2500){
+ return "No can do.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  if (destination === "NYC") {
+    return "Ok, sounds good.";
+}
+else{
+    return "No go.";
+}
+}
+
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
+}
 }
